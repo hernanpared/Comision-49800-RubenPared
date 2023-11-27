bf243d118fe02e61c3a435d6aec0d60ab8164047
ruben hernan pared <hernan2014@hotmail.com>
2023-11-27 20:45:59 -0300

    boton RM

--- js/calculadoraRM.js
+++ js/calculadoraRM.js
@@ -1,4 +1,16 @@
 
 function rm(){
- alert("Hola")
+
+let kg = parseFloat(prompt("Ingre el peso en kilogramos"));
+let rep = parseInt(prompt("Ingrese la cantidad de repeticiones realizadas"));
+
+const num = 30;
+
+
+let rm = kg *(1 + rep / num)
+
+alert(`Tu 1RM = ${rm}`)
+
+
+
 }
